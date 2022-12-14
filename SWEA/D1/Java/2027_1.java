d3f9acc18f5d8d53b776184e8080f6c4e848a6a6
C0012S <alwaysh1002@gmail.com>
2022-12-14 20:53:23 +0900

    [SWEA|Java] D1 - 2027. 대각선 출력하기 (1)

---
+++ SWEA/D1/Java/2027_1.java
@@ -0,0 +1,42 @@
+/*
+2027. 대각선 출력하기
+
+    · 시간 : 1개 테스트케이스를 합쳐서 C++의 경우 30초 / Java의 경우 30초 / Python의 경우 30초
+    · 메모리 : 힙, 정적 메모리 합쳐서 256MB 이내, 스택 메모리 1MB 이내
+
+    
+    주어진 텍스트를 그대로 출력하세요.
+    
+    
+    입력
+    
+    출력
+        #++++
+        +#+++
+        ++#++
+        +++#+
+        ++++#
+*/
+
+
+// 실행 시간 : 0.09924s
+// Pass
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		String[] str = {"+", "+", "+", "+", "+"};
+		
+		for (int i = 0; i < 5; i++) {
+			str[i] = "#";
+			
+			for (int j = 0; j < 5; j++) {
+				System.out.print(str[j]);
+			}
+			System.out.println();
+			
+			str[i] = "+";
+		}
+	}
+}
