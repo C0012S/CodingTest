f2ba6413ceb8a49055c17177844f297730ab6131
C0012S <alwaysh1002@gmail.com>
2023-12-24 16:36:58 +0900

    [BOJ | Java] Bronze5 - 2557. Hello World

---
+++ BOJ/Bronze/Bronze5/Java/2557.java
@@ -0,0 +1,43 @@
+/*
+2557. Bronze 5 - Hello World
+
+    시간 제한	    메모리 제한        제출        정답	      맞힌 사람	    정답 비율
+    1 초	    128 MB           1070074	430976    302452	     39.368%
+
+
+    문제
+        Hello World!를 출력하시오.
+
+
+    입력
+        없음
+
+
+    출력
+        Hello World!를 출력하시오.
+
+
+    예제 입력 1
+
+    예제 출력 1
+        Hello World!
+
+
+    알고리즘 분류
+        구현
+*/
+
+
+// 메모리 : 14144KB
+// 시간 : 120ms
+// 코드 길이 : 189B
+// 정답
+
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Hello World!");
+
+        System.out.println(sb);
+    }
+}
