a138f907b6882e3d232163e24cfb7408d9955628
C0012S <alwaysh1002@gmail.com>
2025-07-23 23:52:36 +0900

    [LeetCode | Oracle] Easy - 1484. Group Sold Products By The Date

--- LeetCode/Easy/Oracle/Group_Sold_Products_By_The_Date.sql
+++ LeetCode/Easy/Oracle/Group_Sold_Products_By_The_Date.sql
@@ -71,6 +71,6 @@ SELECT TO_CHAR(sell_date, 'YYYY-MM-DD') AS sell_date,
        COUNT(product) AS num_sold,
        LISTAGG(product, ',') WITHIN GROUP(ORDER BY product) AS products
 FROM (SELECT DISTINCT product, sell_date
-    FROM Activities)
+      FROM Activities)
 GROUP BY sell_date
 ORDER BY sell_date;
