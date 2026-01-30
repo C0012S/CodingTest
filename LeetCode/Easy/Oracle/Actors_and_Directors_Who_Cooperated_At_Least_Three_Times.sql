b2ccf6c9373ba1f01d2665d45684dcf8beeca80d
C0012S <alwaysh1002@gmail.com>
2026-01-30 23:38:10 +0900

    [LeetCode | Oracle] Easy - 1050. Actors and Directors Who Cooperated At Least Three Times

--- LeetCode/Easy/Oracle/Actors_and_Directors_Who_Cooperated_At_Least_Three_Times.sql
+++ LeetCode/Easy/Oracle/Actors_and_Directors_Who_Cooperated_At_Least_Three_Times.sql
@@ -16,7 +16,7 @@ Easy. #1050 - Actors and Directors Who Cooperated At Least Three Times
         timestamp is the primary key (column with unique values) for this table.
 
 
-    Write a solution to find all the pairs where the actor has cooperated with the director at least three times.(actor_id, director_id)
+    Write a solution to find all the pairs (actor_id, director_id) where the actor has cooperated with the director at least three times.
     Return the result table in any order.
     The result format is in the following example.
 
